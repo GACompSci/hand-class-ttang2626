ad723caf7e0588ecabacc1e07ebe0853badb3aea
Tyler Tang <ttang26@germantownacademy.org>
2025-02-06 00:02:23 -0500

    submiting

--- src/App.java
+++ src/App.java
@@ -1,5 +1,50 @@
+import java.util.ArrayList;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        ArrayList nums = new ArrayList<>();
+        nums.add(10);
+        nums.set(0,2);
+        nums.remove(0);
+        System.out. println(nums);
+        // Use nested loops to create a full deck (plus two Jokers)
+        // printing each card as you create it. 
+
+
+        // for(int i = 1; i <= 6; i++){
+        //     for(int n = 1; n <= 13; n++){
+        //         if(i <= 4){
+        //             Card card = new Card(n, i);
+        //             System.out.println(card.toString());
+        //             // System.out.println(card.getNumCards());
+        //         }
+        //     }
+        //     if (i > 4){
+        //         Card joker = new Card();
+        //         System.out.println(joker.toString());
+        //     }
+
+        // }
+        Deck deck = new Deck(); 
+        
+        // System.out.println(deck.drawCard());
+        // System.out.println(deck.drawCard());
+        // System.out.println(deck);
+
+        Hand hand = new Hand(deck, 4);
+        hand.addCard(2,2);
+        System.out.println(hand.playCard(2,2));
+        System.out.println(hand.cardPresent(5,1));
+
+        
+        hand.printHand();
+        // System.out.println(hand.isSorted());
+        // hand.sortHand();
+        
+        // hand.printHand();
+
+        
+        
+        // System.out.println(Card.getNumCards());
     }
 }
